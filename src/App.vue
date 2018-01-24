1ef27137f3ba3ff4bc090927ab7d5ad2610456d8
ruhao <921401845@qq.com>
2018-01-24 14:56:15 +0800

    test3

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <button @click="getData">click</button>
     <router-link to="/Text">9999</router-link>
     <router-view/>
   </div>
@@ -8,13 +9,29 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      data: {
+        reg: 'lanqiao',
+        password: 'laowaicang',
+        workarea: 'news'
+      }
+    }
+  },
+  methods: {
+    getData () {
+      this.$http.post('http://localhost:3000/admin/data', this.data).then(res => {
+        console.log(res)
+      })
+    }
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
